af3f03583fdf275af7d7c5947c946df9233cb95f
caique vitoriano <caiq-ferre@hotmail.com>
2018-03-26 16:31:52 -0300

    criando modelo supervisor

---
+++ src/modelo/Supervisor.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ *
+ * @author caique
+ */
+public class Supervisor {
+    
+    private String nome;
+    private int codigo;
+    private int cpf;
+    private LocalDate nascimento;
+
+    public Supervisor(String nome, int codigo, int cpf, LocalDate nascimento) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.cpf = cpf;
+        this.nascimento = nascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.nome);
+        hash = 67 * hash + this.codigo;
+        hash = 67 * hash + this.cpf;
+        hash = 67 * hash + Objects.hashCode(this.nascimento);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Supervisor other = (Supervisor) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        if (this.cpf != other.cpf) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.nascimento, other.nascimento)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Supervisor{" + "nome=" + nome + ", codigo=" + codigo + ", cpf=" + cpf + ", nascimento=" + nascimento + '}';
+    }
+    
+    
+    
+    
+}
